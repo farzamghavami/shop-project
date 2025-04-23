a5023890ed5222f00df1a988219ac6dbeb3efbc5
root <root@gmail.com>
2025-04-23 20:34:07 +0330

    adding user field to category model and make migrations

--- ecommerce/orders/models.py
+++ ecommerce/orders/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.db.models.fields import FloatField
-
 from accounts.models import User,Address
 from catalog.models import Product,Shop
 
@@ -8,15 +7,15 @@ class Order(models.Model):
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     address = models.ForeignKey(Address, on_delete=models.CASCADE)
-    total_price = models.DecimalField(decimal_places=2, max_digits=10)
+    total_price = models.DecimalField(decimal_places=2, max_digits=10,default=0)
 
     def __str__(self):
         return f"Order #{self.id} by {self.user.username}"
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE,related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    row_price = models.DecimalField(decimal_places=2, max_digits=10)
+    row_price = models.DecimalField(decimal_places=2, max_digits=10,default=0)
     count = models.IntegerField()
     def __str__(self):
         return f"{self.count} x {self.product.name}"
